241007b996e56f95a06a36af9232b4f74782728d
Manuel-Ventura-Work <manuelventurawork@gmail.com>
2023-05-11 09:47:48 -0300

    Endpoint nuevo (canciones de artista por ID)

--- controllers/audncontrollers.js
+++ controllers/audncontrollers.js
@@ -1,9 +1,23 @@
 const knex = require("../config/knexfile");
 
+
+//Canciones y artistas
 exports.getSongsAndArtists = async (req, res) => {
     try {
 const resultado = await knex.select('*').from("Song").innerJoin("Artists", 'Song.id_artist', 'Artists.id_artist');
     res.status(200).json({SongsArtists: resultado})
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+}
+
+//Canciones de un artista con QUERY
+
+exports.getSongByArtist = async (req, res) => {
+    const queryId = Number(req.query.id);
+    try {
+        const resultado = await knex.select('*').from("Song").innerJoin("Artists", 'Song.id_artist', 'Artists.id_artist').where('Artists.id_artist', queryId);
+        res.status(200).json({resultado})
     } catch (error) {
         res.status(400).json({error: error.message})
     }
--- routes/audnroutes.js
+++ routes/audnroutes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const routes = express.Router();
-const { getSongsAndArtists } = require("../controllers/audncontrollers")
+const { getSongsAndArtists, getSongByArtist } = require("../controllers/audncontrollers")
 
 //Obtener Canciones y Artistas
 
 routes.get("/songartist", getSongsAndArtists);
 
-
+//Obtener canciones de un artista por ID QUERY
+routes.get('/songbyartist', getSongByArtist)
 
 //export
 module.exports = routes;
